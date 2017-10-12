8831ae3d9655e29bb10efe1ab0c5b97ca402e7da
tezaharsony <teza.harsony230394@gmail.com>
2017-10-12 15:25:18 +0700

    populate and start frontend

--- server/controllers/articleControllers.js
+++ server/controllers/articleControllers.js
@@ -17,25 +17,59 @@ var createArticle = function(req,res){
   })
 }
 
+var getAllArticles = function (req,res) {
+  Article.find({}, (err, article) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(article)
+    }
+  })
+  .populate({path:'author', model:'users'})
+}
+
+var getOneArticle = function (req,res) {
+  Article.findOne({_id:req.params.id}, (err, article) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(article)
+    }
+  })
+}
+
+var getOneArticleByAuthor = function (req,res) {
+  Article.findOne({author:req.params.author}, (err, article) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(article)
+    }
+  })
+  .populate({path:'author', model:'users'})
+}
+
+var getOneArticleByCategory = function (req,res) {
+  Article.findOne({category:req.params.category}, (err, article) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(article)
+    }
+  })
+}
+
+
+var removeSpecifiedArticle = function (req,res) {
+  Article.findOneAndRemove({_id:req.params.id}, (err,article) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(article)
+    }
+  })
+}
+
 module.exports = {
-  createArticle
+  createArticle,getAllArticles,getOneArticle,getOneArticleByAuthor,getOneArticleByCategory,removeSpecifiedArticle
 };
-
-
-
-
-// var create = function(req, res) {
-//   let newThread = new Thread({
-//     title: req.body.title,
-//     thread: req.body.thread,
-//     createdAt: new Date(),
-//     user_id: req.headers.authentic.id
-//   })
-//   newThread.save((err, createdThread) => {
-//     if(err) {
-//       res.send(err)
-//     } else {
-//       res.send(createdThread)
-//     }
-//   })
-// }
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -19,5 +19,10 @@ const auth = (req, res, next) =>
 }
 
 router.post('/', auth, articleCont.createArticle)
+router.get('/', auth, articleCont.getAllArticles)
+router.get('/:id/getone', auth, articleCont.getOneArticle)
+router.get('/:author/getbyauthor', auth, articleCont.getOneArticleByAuthor)
+router.get('/:category/getbycategory', auth, articleCont.getOneArticleByCategory)
+router.delete('/:id', auth, articleCont.removeSpecifiedArticle)
 
 module.exports = router
